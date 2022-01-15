2970712b2a17f072ed62005e6dd54ec2b66f12e5
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-01-15 16:14:34 -0300

    Props

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,10 +7,10 @@ function NavBar() {
 			<nav className='container'>
 				<div className='dsmovie-nav-content'> 
 					<h1>DSMovie</h1>
-					<a href='https://github.com/devsuperior' target="_blank" rel="noreferrer noopener">
+					<a href='https://github.com/matheustirabassi' target="_blank" rel="noreferrer noopener">
 						<div className='dsmovie-contact-container'>
 							<GithubIcon />
-							<p className='dsmovie-contact-link'>/devsuperior</p>
+							<p className='dsmovie-contact-link'>/matheustirabassi</p>
 						</div>
 					</a>
 				</div>
