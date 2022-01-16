e89ae874e31c6a67d8ae639e47a088d8664ea033
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-01-16 14:56:46 -0300

    pagination
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,21 @@
 import { ReactComponent as Arrow } from 'assets/img/arrow.svg'
+import { MoviePage } from 'types/movie';
 import './styles.css'
 
-function Pagination() {
+type Props = {
+	page: MoviePage;
+	onChange: Function;
+}
+
+function Pagination({page, onChange} : Props) {
 	return (
 		<div className="dsmovie-pagination-container">
 			<div className="dsmovie-pagination-box">
-				<button type="button" className="dsmovie-pagination-button" aria-label="btn-back" disabled={true}>
+				<button type="button" className="dsmovie-pagination-button" aria-label="btn-back" disabled={page.first} onClick={() => onChange(page.number - 1)}>
 					<Arrow />
 				</button>
-				<p>{`${1} de ${3}`}</p>
-				<button type="button" className="dsmovie-pagination-button" aria-label="btn-next" disabled={false}>
+				<p>{`${page.number + 1} de ${page.totalPages}`}</p>
+				<button type="button" className="dsmovie-pagination-button" aria-label="btn-next" disabled={page.last} onClick={() => onChange(page.number + 1)}>
 					<Arrow className="dsmovie-flip-horizontal" />
 				</button>
 			</div>
